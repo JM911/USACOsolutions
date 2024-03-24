70708359e459a17e294810cda620bb0cf2ec20ee
JM-Laptop <jmjeon911@gmail.com>
2024-03-24 12:48:15 +0900

    USACO 용으로 템플릿 변경

--- Template.cpp
+++ Template.cpp
@@ -1,10 +1,20 @@
-#define FASTIO cin.tie(nullptr); ios_base::sync_with_stdio(false)
 #include "bits/stdc++.h"
 using namespace std;
 
+void setIO(string name = "") 
+{
+    cin.tie(nullptr);
+    ios_base::sync_with_stdio(false);
+
+	if (!name.empty()) {
+		freopen((name + ".in").c_str(), "r", stdin);
+		freopen((name + ".out").c_str(), "w", stdout);
+	}
+}
+
 int main()
 {
-	FASTIO;
+	setIO("");
 
 	
 }
